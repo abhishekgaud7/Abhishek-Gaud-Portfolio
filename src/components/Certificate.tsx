03e596ec0d731d4980a08df1ccc056052ad32aee
Abhishek Gaud <romanreings59368@gmail.com>
2025-12-31 22:38:39 +0530

    Refactor: Extract Certificate data/type and add image titles

--- src/components/Certificate.tsx
+++ src/components/Certificate.tsx
@@ -1,46 +1,6 @@
 import React from 'react'
+import { certificateData, certificateImages } from '../data/certificates'
 import './Certificate.css'
-
-interface CertificateItem {
-  school: string
-  qualification: string
-  location: string
-  details: string[]
-  meta?: string
-}
-
-interface CertificateImage {
-  id: string
-  title: string
-  image: string
-  issuer?: string
-  date?: string
-}
-
-// Additional Courses/Certifications (like TOPS Technologies)
-const certificateData: CertificateItem[] = [
-  {
-    school: 'Tops Technologies',
-    qualification: 'Full Stack Java Developer',
-    location: 'Ahmedabad, India',
-    details: [
-      'Completed an intensive Full Stack Java Development course.',
-      'Skills: C, C++, SQL, HTML, CSS, JavaScript, Bootstrap, jQuery, React.js, Java, JSP, Servlet, JDBC, Spring Boot, Hibernate, Microservices, RESTful API.',
-      'Winner of multiple coding events and competitions.'
-    ],
-    meta: 'Additional Course'
-  },
-]
-
-// Certificate Images Gallery
-// Yahan aap certificate images add kar sakte hain
-const certificateImages: CertificateImage[] = [
-  {
-    id: 'cert-1',
-    title: 'Certificate of Training Completion',
-    image: '/assets/acmegrade-cert.jpg',
-  },
-]
 
 const Certificate: React.FC = () => {
   return (
@@ -83,7 +43,7 @@ const Certificate: React.FC = () => {
             <div className="certificate-gallery">
               {certificateImages.map((cert) => (
                 <div key={cert.id} className="certificate-image-card">
-                  <div className="certificate-image-wrapper">
+                  <div className="certificate-image-wrapper" title={cert.title}>
                     <img
                       src={cert.image}
                       alt={cert.title}
