3ba9520861322061d065e86abadba657171312fd
Abhishek Gaud <romanreings59368@gmail.com>
2026-01-18 18:54:29 +0530

    feat: centralize social links in data/socials.ts

---
+++ src/data/socials.ts
@@ -0,0 +1,7 @@
+export const SOCIALS = {
+    github: 'https://github.com/abhishekgaud7',
+    linkedin: 'https://www.linkedin.com/in/abhishek-gaud-cse',
+    email: 'ritikempire505@gmail.com',
+    phone: '+91 91712 14043',
+    phoneTel: '+919171214043',
+}
